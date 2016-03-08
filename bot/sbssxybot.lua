bdc3e6131f6d3259a74c0dd26ee9b8789fef1c28
elt11bot <aryaneskandari1381@gmail.com>
2016-03-09 00:41:22 +0330

    Update sbssxybot.lua

--- bot/sbssxybot.lua
+++ bot/sbssxybot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "moderation",
       "plugins",
       "version"},
-    sudo_users = {109722284,119989724},
+    sudo_users = {53406884},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
@@ -299,7 +299,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_id = 53406884
 now = os.time()
 math.randomseed(now)
 started = false
